a127ccacd959a4f6e05fa84c18357ffdcd956080
Mickey Camarco <48422179+mcamarco@users.noreply.github.com>
2023-06-09 15:26:48 -0400

    updated about

--- src/components/about.js
+++ src/components/about.js
@@ -12,20 +12,18 @@ function About() {
             </div>
             <div className="col-md-9">
               <p>
-                Mickey Camarco is a dedicated professional with a passion for technology and a drive for success. With a strong background in technical channel enablement, pre-sales solutions engineering, and business analysis, they bring a wealth of experience to the table. They have a proven track record of exceeding sales targets, driving partner-sourced revenue, and building strategic partnerships.
+                I am a dedicated professional with a passion for technology and a drive for success. With a strong background in technical channel enablement, pre-sales solutions engineering, and business analysis, I bring a wealth of experience to the table. Throughout my career, I have consistently exceeded sales targets, driven partner-sourced revenue, and built strategic partnerships.
               </p>
               <p>
-                Throughout their career, Mickey has honed their skills in delivering compelling technical presentations, influencing product roadmaps, and providing innovative solutions to complex problems. They thrive on challenges and continuously seek opportunities to expand their knowledge and expertise. With a keen eye for detail and a strong analytical mindset, they consistently deliver high-quality results.
+                I excel in delivering compelling technical presentations, influencing product roadmaps, and providing innovative solutions to complex problems. I thrive on challenges and continuously seek opportunities to expand my knowledge and expertise. With a keen eye for detail and a strong analytical mindset, I consistently deliver high-quality results.
               </p>
               <p>
-                Mickey recently completed the rigorous coursework in full-stack web development at the Penn LPS Coding Boot Camp, further expanding their technical skill set. Through this immersive program, they demonstrated proficiency in front-end and back-end technologies, developed dynamic web applications using modern frameworks and libraries, and applied industry best practices and coding standards.
+                Recently, I completed the rigorous coursework in full-stack web development at the Penn LPS Coding Boot Camp, further expanding my technical skill set. Through this immersive program, I demonstrated proficiency in front-end and back-end technologies, developed dynamic web applications using modern frameworks and libraries, and applied industry best practices and coding standards.
               </p>
               <p>
-                With their newly acquired knowledge and hands-on experience, Mickey is well-equipped to tackle diverse technical challenges and contribute to innovative projects. They are excited to apply their skills and continue their journey of growth and learning in the field of web development. They look forward to connecting with like-minded professionals and exploring new opportunities to contribute their skills and expertise.
+                With my newly acquired knowledge and hands-on experience, I am well-equipped to tackle diverse technical challenges and contribute to innovative projects. I am excited to apply my skills and continue my journey of growth and learning in the field of web development. I look forward to connecting with like-minded professionals and exploring new opportunities to contribute my skills and expertise.
               </p>
-              <p>
-                Let's connect and embark on an exciting journey together!
-              </p>
+              <p>Let's connect and embark on an exciting journey together!</p>
 
             </div>
           </div>
